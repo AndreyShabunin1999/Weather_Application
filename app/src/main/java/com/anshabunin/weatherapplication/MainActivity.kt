5e2cc1d777674584cdcce3bb0a5c20563f5a40ec
AndreyShabunin1999 <81184064+AndreyShabunin1999@users.noreply.github.com>
2024-02-06 01:16:35 +0300

    Сделан Full экран

--- app/src/main/java/com/anshabunin/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/anshabunin/weatherapplication/MainActivity.kt
@@ -2,12 +2,21 @@ package com.anshabunin.weatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_main)
     }
 }
